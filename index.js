05a6a58e09495e43c7626cd86f4933958317c4dd
Guo Yunhe <guoyunhebrave@gmail.com>
2017-08-17 01:29:26 +0300

    Basically useful

---
+++ index.js
@@ -0,0 +1,34 @@
+var FEEDSUB = require('feedsub');
+var IRC = require('irc');
+
+
+// Set up your little spambot
+var server = 'chat.freenode.net';
+var bot = 'suse_cn_forum';
+var channel = '#opensuse-cn';
+var feed = 'https://forum.suse.org.cn/feed.php';
+var interval = 1 // how often to poll the feed, in minutes?
+
+
+console.log('start irc client');
+
+var client = new IRC.Client(server, bot,
+  {
+    channels: [channel],
+    realName: 'nodejs IRC bot',
+    autoRejoin: true,
+    autoConnect: true,
+  });
+
+console.log('start feed client');
+
+var reader = new FEEDSUB(feed, {
+  interval: interval,
+  autoStart: true,
+
+});
+
+reader.on('item', function (item) {
+  console.log(item.title);
+  client.say(channel, item.title + ' ' + item.link.href);
+});
