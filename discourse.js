de1df3ead89d640b7c1f15a137eb953a056f1f85
Guo Yunhe <i@guoyunhe.me>
2019-04-17 11:32:40 +0300

    Fetch JSON API

---
+++ discourse.js
@@ -0,0 +1,32 @@
+const fetch = require('node-fetch');
+
+module.exports = function (send, name, url, interval) {
+    let timestamp;
+    setInterval(function () {
+        fetch(url + 'posts.json')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                if (timestamp) {
+                    data.latest_posts.filter(function (post) {
+                        return post.created_at > timestamp;
+                    })
+                    .reverse()
+                    .map(function (post) {
+                        let message = '[' + name + '] ✉️ ';
+                        if (post.post_number > 1) {
+                            message += 'Re:'
+                        }
+                        message += post.topic_title + ' (' + post.name + ') ';
+                        message += `${url}t/topic/${post.topic_id}/${post.post_number}`;
+                        send(message);
+                    });
+                }
+
+                timestamp = data.latest_posts[0].created_at;
+            }).catch(function (e) {
+                console.error(e);
+            });
+    }, 1000 * interval);
+};
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const FeedSub = require('feedsub');
 const IRC = require('irc');
-const config = require('./config.js');
+const config = require('./config');
+const discourse = require('./discourse');
 
 const client = new IRC.Client(config.irc.server, config.irc.nickname, {
   channels: [config.irc.channel],
@@ -16,19 +16,15 @@ client.addListener('message', function (from, to, message) {
 client.addListener('error', function(message) {
     console.log('error: ', message);
 });
+    
+function send(message) {
+    client.say(config.irc.channel, message);
+}
 
-console.log('feeds');
 config.feeds.map(function (feed) {
-    let reader = new FeedSub(feed.url, {
-        interval: feed.interval,
-        forceInterval: true,
-        maxHistory: 100
-    });
-
-    reader.on('item', function (item) {
-        console.log(item.title);
-        client.say(config.irc.channel, '[' + feed.name + '] ' + item.title + ' (' + item['dc:creator'] + ') ' + item.link);
-    });
-
-    reader.start();
+    switch (feed.type) {
+        case 'discourse':
+            discourse(send, feed.name, feed.url, feed.interval);
+            break;
+    }
 });
